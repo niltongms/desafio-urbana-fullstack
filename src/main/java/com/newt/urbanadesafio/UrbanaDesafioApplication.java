4142ac1d9c3cda9f310651d8b7c54b4ba7f36b5d
Elenilton Gomes <niltonsgms@gmail.com>
2026-01-28 21:35:16 -0300

    feat: configuração inicial do projeto com spring boot e postgres

---
+++ src/main/java/com/newt/urbanadesafio/UrbanaDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.newt.urbanadesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanaDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanaDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newt/urbanadesafio/UrbanaDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newt.urbanadesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanaDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

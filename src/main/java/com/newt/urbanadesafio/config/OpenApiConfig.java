2b97a6ddf2524cc5bce06fe1e9a945059b83272c
Elenilton Gomes <niltonsgms@gmail.com>
2026-01-31 18:54:48 -0300

    feat: adicionando documentação swagger openapi

---
+++ src/main/java/com/newt/urbanadesafio/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.newt.urbanadesafio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Urbana Desafio API")
+                        .description("API para gestão de usuários e cartões de transporte.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Elenilton Gomes")
+                                .email("niltonsgms@gmail.com")
+                        )
+                );
+    }
+}

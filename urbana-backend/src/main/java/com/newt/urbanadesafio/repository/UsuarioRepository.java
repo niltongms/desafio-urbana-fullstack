83c2f323f05e2fb2b480db5d0a8fdc1a3f7ae610
Elenilton Gomes <niltonsgms@gmail.com>
2026-02-01 13:31:27 -0300

    refactor(repository): altera retorno de busca por email para Optional para evitar nulos

--- urbana-backend/src/main/java/com/newt/urbanadesafio/repository/UsuarioRepository.java
+++ urbana-backend/src/main/java/com/newt/urbanadesafio/repository/UsuarioRepository.java
@@ -3,9 +3,10 @@ package com.newt.urbanadesafio.repository;
 import com.newt.urbanadesafio.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    //  buscar email
-    Usuario findByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
 }

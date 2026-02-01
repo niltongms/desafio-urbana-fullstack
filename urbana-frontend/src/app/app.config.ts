06764a31ce8c3e6f7a6596c1eec0c04c8d540ed4
Elenilton Gomes <niltonsgms@gmail.com>
2026-02-01 15:35:06 -0300

    chore(deps): instala e configura ngx-toastr e sweetalert2 para notificacoes

--- urbana-frontend/src/app/app.config.ts
+++ urbana-frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'; 
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 
@@ -9,6 +10,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations() 
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000, // Fica na tela por 3 segundos
+      positionClass: 'toast-top-right', // Aparece no canto superior direito
+      preventDuplicates: true,
+      progressBar: true 
+      })
   ]
 };

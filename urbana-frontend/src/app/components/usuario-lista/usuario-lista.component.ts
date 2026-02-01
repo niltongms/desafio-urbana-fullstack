2373326aabd891fbec7d0b9fa05ec8e02ef8a1a5
Elenilton Gomes <niltonsgms@gmail.com>
2026-02-01 11:17:38 -0300

    feat(frontend): finaliza integracao da tabela de usuarios com a api

--- urbana-frontend/src/app/components/usuario-lista/usuario-lista.component.ts
+++ urbana-frontend/src/app/components/usuario-lista/usuario-lista.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.interface';
 
 @Component({
   selector: 'app-usuario-lista',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './usuario-lista.component.html',
   styleUrl: './usuario-lista.component.scss'
 })
-export class UsuarioListaComponent {
+export class UsuarioListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
 
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.carregarUsuarios();
+  }
+
+  carregarUsuarios() {
+    this.usuarioService.listarTodos().subscribe({
+      next: (dados) => {
+        this.usuarios = dados;
+        console.log('Sucesso! Dados recebidos:', dados);
+      },
+      error: (erro) => {
+        console.error('Erro:', erro);
+        alert('Erro ao buscar usuários. Verifique se o Backend Java está rodando!');
+      }
+    });
+  }
 }
